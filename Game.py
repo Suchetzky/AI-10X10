d789453379585cacbe28c64f6ae7b48c722cc851
Eitam Katz <eitam.katz@mail.huji.ac.il>
2024-09-13 16:27:42 +0300

    move a* to another file

--- Game.py
+++ Game.py
@@ -291,26 +291,7 @@ def breadth_first_search(problem):
     queue = Queue()
     return bfs_dfs_helper(queue, problem)
 
-def a_star_search(problem, heuristic=None):
-    return a_star_helper(util.PriorityQueue(), problem, heuristic)
 
-def a_star_helper(data_type, game, heuristic): # todo check
-    data_type.push((game, []), 0)
-    visited = set()
-
-    while not data_type.isEmpty():
-        state, path= data_type.pop()
-
-        if state.is_goal_state():
-            return path
-        
-        if state not in visited:
-            visited.add(state)
-            for successor, actionNode in state.get_successors():
-                if successor not in visited:
-                    new_path = path + [actionNode]
-                    data_type.push((successor, new_path), -successor.get_score())
-    return []
 
 def bfs_dfs_helper(data_type, game): # todo check
     data_type.push((game, [], []))
@@ -353,7 +334,7 @@ def track_memory_and_time_for_dfs(game_instance):
 
     # Run depth_first_search
     solution_path, grid = depth_first_search(game_instance)
-    # solution_path = a_star_search(game_instance)
+    solution_path = Astar.a_star_search(game_instance, Heuristics.heuristic)
     # Stop the timer
     # Heuristics.write_weights_to_csv(score)
     end_time = time.time()
