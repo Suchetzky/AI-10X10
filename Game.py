b116720332df62730ca15475f9d31be9b24fff7f
Halel Katz <halelkatz123@cs.huji.ac.il>
2024-09-19 22:10:01 +0300

    interface

--- Game.py
+++ Game.py
@@ -16,8 +16,10 @@ import multi_agents
 # Set up display dimensions
 screen_width, screen_height = 900, 700
 
+
 class Game:
-    def __init__(self, NoUI=False, board_len=10, size=50, test=False, sleep_between_actions=False):
+    def __init__(self, NoUI=False, board_len=10, size=50, test=False,
+                 sleep_between_actions=False):
         self.board_len = board_len
         self.size = size
         self.grid = Grid(board_len, board_len, size)
@@ -25,14 +27,16 @@ class Game:
         if not test:
             self.add_next_shapes()
         else:
-            self.next_shapes = [Shape(specific_shape_num=0), Shape(specific_shape_num=16), Shape(specific_shape_num=9)]
+            self.next_shapes = [Shape(specific_shape_num=0),
+                                Shape(specific_shape_num=16),
+                                Shape(specific_shape_num=9)]
         self.sleep_between_actions = sleep_between_actions
         self.placed_pieces = 0
         self.piece_num = 0
         self.current_x = 0
         self.current_y = 0
         self.score = 0
-        self.headless = NoUI # game is runninf with or without graphical interface
+        self.headless = NoUI  # game is runninf with or without graphical interface
         self.running = True
         self.goal_state = 10000
         if not self.headless:
@@ -43,7 +47,8 @@ class Game:
     def _setup_ui(self):
         self.root = tk.Tk()
         self.root.title('10x10 Game')
-        self.canvas = tk.Canvas(self.root, width=screen_width, height=screen_height, bg='black')
+        self.canvas = tk.Canvas(self.root, width=screen_width,
+                                height=screen_height, bg='black')
         self.canvas.pack()
         self.font = tkFont.Font(family="Helvetica", size=20)
         self.root.bind('<Left>', self.move_left)
@@ -59,12 +64,17 @@ class Game:
         self.canvas.delete('all')
         self.grid.draw(self.canvas)
         if self.next_shapes:
-            self.next_shapes[self.piece_num].draw(self.canvas, self.current_x, self.current_y, self.grid.size)
+            self.next_shapes[self.piece_num].draw(self.canvas, self.current_x,
+                                                  self.current_y,
+                                                  self.grid.size)
             # self.root.update()
             for i in range(len(self.next_shapes)):
-                self.next_shapes[i].draw(self.canvas, 11, (i*6), self.grid.size)
+                self.next_shapes[i].draw(self.canvas, 11, (i * 6),
+                                         self.grid.size)
                 # self.root.update()
-        self.canvas.create_text(self.board_len, screen_height - 40, anchor='nw', text=f'Score: {self.score}', fill='white', font=self.font)
+        self.canvas.create_text(self.board_len, screen_height - 40, anchor='nw',
+                                text=f'Score: {self.score}', fill='white',
+                                font=self.font)
 
         self.root.update()
 
@@ -83,12 +93,13 @@ class Game:
         self._move_piece(0, -1)
 
     def _move_piece(self, dx, dy):
-        if self.grid.can_place(self.next_shapes[self.piece_num].shape, self.current_x + dx, self.current_y + dy):
+        if self.grid.can_place(self.next_shapes[self.piece_num].shape,
+                               self.current_x + dx, self.current_y + dy):
             self.current_x += dx
             self.current_y += dy
             self.draw()
 
-    def rotate_piece(self, event): # todo change shape?
+    def rotate_piece(self, event):
         if self.next_shapes:
             self.piece_num = (self.piece_num + 1) % len(self.next_shapes)
             self.draw()
@@ -105,10 +116,9 @@ class Game:
                 new_shape = Shape()
             self.next_shapes.append(new_shape)
 
-
-        # get coordinates and check if the placement of self.next_shapes[self.piece_num] is valid
-        # if so, place the shape in the board and clear lines if needed and update the score
-    def place_part_in_board_if_valid_by_shape(self, action):  # todo check
+    # get coordinates and check if the placement of self.next_shapes[self.piece_num] is valid
+    # if so, place the shape in the board and clear lines if needed and update the score
+    def place_part_in_board_if_valid_by_shape(self, action):
         if action is None:
             return
         x, y, piece_num, next_shapes = action[1].action
@@ -120,7 +130,7 @@ class Game:
             self.score += shape.get_part_size()
             self.grid.place_shape(shape.shape, x, y)
             self.score += self.grid.clear_lines()
-            self.next_shapes.pop(self.next_shapes.index(shape)) # todo we have a problem here
+            self.next_shapes.pop(self.next_shapes.index(shape))
             self.placed_pieces += 1
             if self.placed_pieces == 3:
                 self.add_next_shapes()
@@ -129,13 +139,12 @@ class Game:
             else:
                 self.piece_num = min(self.piece_num, len(self.next_shapes) - 1)
             self.current_x, self.current_y = 0, 0
-            # self.draw() #todo why her?
-
 
     # get coordinates and check if the placement of self.next_shapes[self.piece_num] is valid
     # if so, place the shape in the board and clear lines if needed and update the score
-    def place_part_in_board_if_valid(self, x, y): # todo check
-        if self.grid.can_place(self.next_shapes[self.piece_num].shape, x, y, check_placement=True):
+    def place_part_in_board_if_valid(self, x, y):
+        if self.grid.can_place(self.next_shapes[self.piece_num].shape, x, y,
+                               check_placement=True):
             self.score += self.next_shapes[self.piece_num].get_part_size()
             self.grid.place_shape(self.next_shapes[self.piece_num].shape, x, y)
             self.score += self.grid.clear_lines()
@@ -156,7 +165,7 @@ class Game:
                 return True
         return False
 
-    def get_board_available_places(self, shape): # for bfs/dfs
+    def get_board_available_places(self, shape):  # for bfs/dfs
         available_places = []
         for y in range(self.grid.height):
             for x in range(self.grid.width):
@@ -181,8 +190,11 @@ class Game:
         self.canvas.delete('all')
         game_over_text = "Game Over"
         score_text = f'Score: {self.score}'
-        self.canvas.create_text(screen_width // 2, screen_height // 2 - 50, text=game_over_text, fill='white', font=self.font)
-        self.canvas.create_text(screen_width // 2, screen_height // 2, text=score_text, fill='white', font=self.font)
+        self.canvas.create_text(screen_width // 2, screen_height // 2 - 50,
+                                text=game_over_text, fill='white',
+                                font=self.font)
+        self.canvas.create_text(screen_width // 2, screen_height // 2,
+                                text=score_text, fill='white', font=self.font)
         self.root.update()
         self.root.after(3000, self.root.quit)
 
@@ -200,9 +212,9 @@ class Game:
             self.next_shapes = node.action[3]
             self.draw()
             self.place_part_in_board_if_valid(node.action[0], node.action[1])
-            if self.sleep_between_actions: # brake between steps
+            if self.sleep_between_actions:  # brake between steps
                 time.sleep(1)
-            if not self.headless: # todo: what is this?
+            if not self.headless:
                 self.root.after(1000, self.root.update_idletasks)
         if self.is_game_over():
             self.display_game_over()
@@ -232,21 +244,23 @@ class Game:
         valid_placements = []
         for y in range(self.grid.height):
             for x in range(self.grid.width):
-                if self.grid.can_place(self.next_shapes[self.piece_num].shape, x, y):
+                if self.grid.can_place(self.next_shapes[self.piece_num].shape,
+                                       x, y):
                     valid_placements.append((x, y))
         return valid_placements
 
     def get_successors(self):
         successors = []
-        if len(self.next_shapes) == 0: # just if we finish the shapes batch we will randomly add 3 new shapes
-            self.add_next_shapes() # todo: why its always 3?
+        if len(self.next_shapes) == 0:  # just if we finish the shapes batch we will randomly add 3 new shapes
+            self.add_next_shapes()
         for piece_num in range(len(self.next_shapes)):
-            for x, y in self.get_board_available_places(self.next_shapes[piece_num].shape):
+            for x, y in self.get_board_available_places(
+                    self.next_shapes[piece_num].shape):
                 new_game = self.deepcopy()
                 new_game.piece_num = piece_num
                 new_game.place_part_in_board_if_valid(x, y)
-                successors.append((new_game, Node((x, y, piece_num, self.next_shapes)))) # todo : why tuple?
-        # print(len(successors)) # todo remove
+                successors.append((new_game, Node(
+                    (x, y, piece_num, self.next_shapes))))
         return successors
 
     def is_goal_state(self):
@@ -277,20 +291,16 @@ class Game:
 
     #### for agents ####
 
-
-
     def generate_successor(self, action):
         util.raiseNotDefined()
         # successor = self.deepcopy()
         # successor.place_part_in_board_if_valid(action[0], action[1])
         # return successor
-    
+
     def __lt__(self, other):
         return self.get_score() < other.get_score()
 
     #############################
-
-
 
 
 ######################## DFS BFS ########################
@@ -298,13 +308,13 @@ def depth_first_search(problem):
     stack = Stack()
     return bfs_dfs_helper(stack, problem)
 
+
 def breadth_first_search(problem):
     queue = Queue()
     return bfs_dfs_helper(queue, problem)
 
 
-
-def bfs_dfs_helper(data_type, game): # todo check
+def bfs_dfs_helper(data_type, game):
     data_type.push((game, [], []))
     visited = set()
 
@@ -321,22 +331,17 @@ def bfs_dfs_helper(data_type, game): # todo check
                     data_type.push((successor, new_path, new_grid))
     return [], []
 
+
 ########################################################
-
-# def print_path(grid):
-#     for node in grid:
-#         for row in node:
-#             print(row)
-#         print("\n")
-#         # print(node, "\n")
-
 
 ### For data collection
 import tracemalloc
 import time
 import pandas as pd
 
-def track_memory_and_time_for_dfs(game_instance, format='DFS', agents='AlphaBetaAgent', draw=True):
+
+def track_memory_and_time_for_dfs(game_instance, format='DFS',
+                                  agents='AlphaBetaAgent', draw=True):
     # Start tracing memory allocations
     Heuristics.random_weights()
     tracemalloc.start()
@@ -364,6 +369,7 @@ def track_memory_and_time_for_dfs(game_instance, format='DFS', agents='AlphaBeta
     # Return the time taken and peak memory usage
     return end_time - start_time, peak / 1024 / 1024  # time in seconds, memory in MB
 
+
 def run_multiple_times(game_instance):
     # List to store the results
     results = []
@@ -372,13 +378,15 @@ def run_multiple_times(game_instance):
         # Recreate the game instance each time (to reset the game state)
         new_game_instance = game_instance.deepcopy()
         # Track memory and time for the current run
-        time_taken, memory_used = track_memory_and_time_for_dfs(new_game_instance)
+        time_taken, memory_used = track_memory_and_time_for_dfs(
+            new_game_instance)
 
         # Append the results (run number, time, memory)
         results.append([time_taken, memory_used])
 
     # Convert results to a DataFrame
-    df = pd.DataFrame(results, columns=["Time Taken (seconds)", "Memory Used (MB)"])
+    df = pd.DataFrame(results,
+                      columns=["Time Taken (seconds)", "Memory Used (MB)"])
 
     # Calculate the averages
     avg_time = df["Time Taken (seconds)"].mean()
@@ -390,32 +398,46 @@ def run_multiple_times(game_instance):
     # Return the averages
     return avg_time, avg_memory
 
+
 def main():
     parser = argparse.ArgumentParser(description='10x10 Game')
     format = ['play', 'DFS', 'A*', 'agents']
-    parser.add_argument("--format", help="choose format", type=str, default='play', choices=format)
+    parser.add_argument("--format", help="choose format", type=str,
+                        default='play', choices=format)
     args = parser.parse_args()
     if args.format == 'play':
         initial_game = Game(False, 10, 50, False, True)
         initial_game.run()
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='10x10 Game')
     format = ['play', 'DFS', 'A_star', 'agent']
     agents = ['GreedyAgent', 'AlphaBetaAgent', 'ExpectimaxAgent']
-    parser.add_argument("--display", help="The game UI. True for GUI False otherwise", type=bool, default=False)
-    parser.add_argument("--format", help="choose format: 'play', 'DFS', 'A_star', 'agents'", type=str, default='play', choices=format)
-    parser.add_argument('--agent', choices=agents, help='The agent. default is AlphaBetaAgent', default=agents[1], type=str)
-    parser.add_argument('--depth', help='The maximum depth for to search in the game tree.',
+    parser.add_argument("--display",
+                        help="The game UI. True for GUI False otherwise",
+                        type=bool, default=False)
+    parser.add_argument("--format",
+                        help="choose format: 'play', 'DFS', 'A_star', 'agents'",
+                        type=str, default='play', choices=format)
+    parser.add_argument('--agent', choices=agents,
+                        help='The agent. default is AlphaBetaAgent',
+                        default=agents[1], type=str)
+    parser.add_argument('--depth',
+                        help='The maximum depth for to search in the game tree.',
                         default=1, type=int)
     parser.add_argument('--sleep_between_actions',
-                        help='Should sleep between actions.', default=False, type=bool)
-    parser.add_argument('--score_goal', help='The score goal to reach. for DFS and A_star', default=10000, type=int)
+                        help='Should sleep between actions.', default=False,
+                        type=bool)
+    parser.add_argument('--score_goal',
+                        help='The score goal to reach. for DFS and A_star',
+                        default=10000, type=int)
     args = parser.parse_args()
-    initial_game = Game(not args.display, 10, 50, False, args.sleep_between_actions)
+    initial_game = Game(not args.display, 10, 50, False,
+                        args.sleep_between_actions)
     initial_game.goal_state = args.score_goal
     if args.format == 'play':
-            initial_game.run()
+        initial_game.run()
     elif args.format == 'DFS':
         solution_path, grid = depth_first_search(initial_game)
         if args.display:
@@ -435,7 +457,6 @@ if __name__ == '__main__':
         game_runner = multi_agents.Game_runner(agent, agent, draw=args.display)
         score = game_runner.run(initial_game)
         print(score)
-
 
     # avg_time, avg_memory = run_multiple_times(initial_game, 100)
     # #solution_path, score = a_star_search(initial_game)
